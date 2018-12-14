015c69b054ac2936305b85e69965572ef71e8308
tiramisubliss <wasestoh@gmail.com>
2018-12-14 18:00:04 +0700

    jwtrails

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::API
+  
+  def not_found
+        render json: { error: 'not_found' }
+  end
+  
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find_by_id(@decoded[:user_id])
+    rescue JWT::DecodeError
+      render json: { error: 'unauthorized' }, status: :unauthorized
+  end
+ end
+end 
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :username, uniqueness: true
+  validates :password,
+            length: {minimum: 6},
+            confirmation: true,
+            if: lambda{new_record? || !password.nil?
+    }
+end
